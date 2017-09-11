4053bdb47e8c7adc3b0b60259e6857c7011c956f
Kyle M Foss <fossx229@bebop.morris.umn.edu>
2017-09-11 00:47:52 -0500

    --Added documentation

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 logfiles=$1
 
+#Change into the directory we want to work in
 cd $logfiles
 
-#This works in the commandline but breaks in the shell file not sure why.
 
+#Use the grep command for all the log files and write to a txt file
 grep -rh "Failed password for" *  | cat > name.txt
+#This command replaces invalid user with an empty string
 sed -i 's/invalid user //g' name.txt
-
+#write the columns we ant to a file
 awk '{print $1, $2, substr($3, 0, 2), $9, $11 }' name.txt > failed_login_data.txt
-
+#remove this file
 rm name.txt
