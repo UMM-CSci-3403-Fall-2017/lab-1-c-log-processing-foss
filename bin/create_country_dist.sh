d97cf0317b285dce296505c88a52a60d722dc4f0
Kyle M Foss <fossx229@acrylic.morris.umn.edu>
2017-09-12 15:18:20 -0500

    --The file takes an argument which presumably be a directory that will contain the files we need to the file work. --In general, the file will use the failed_login_data.txt and extract the ip addresses and sort the addresses. --The sort will allow a join command to be used to match the ip addresses with different countries. --The result from the join command is fed into a command that counts the frequencey of the countries and places them into a javascript function. --The javascript function is then wrapped into html tags that will generate the google chart.

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -4,18 +4,20 @@
 #grabs the location of the file we want
 pathName=$(find $1 -type f -name "*failed_login_data.txt")
 
-#grabs the infromation we want, in this case it's users
+#grabs the infromation we want, in this case it's ip addresses
 awk '{print $5}' $pathName  > intermediate.txt
 
-#sorts the data lexographically and counts the frequencey of the users and prints the data to a javascript fucntion and finally write it to a document.
+#sorts the ips so a join command can be used on it
 sort  intermediate.txt > failtest.txt
 
+#This joins the first and the second file together according to their ip
 join -1 1 -2 1 failtest.txt ./etc/country_IP_map.txt > results.txt
 
+#grabs the ip addresses with the country codes
 awk '{print $2}' results.txt > ips.txt
 
-
-uniq -c ips.txt | awk '{print "data.addRow([\x27"$2"\x27, " $1"]);";}' > fail.txt
+#Sorts the ips with the country codes and counts the frequencey of the countires pipes the results to a command that places the results in a javascript function. The command then write the result to a text document.
+sort ips.txt | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, " $1"]);";}' > fail.txt
 
 #wraps the text in html tags
 ./bin/wrap_contents.sh fail.txt ./html_components/country_dist $1/country_dist.html
