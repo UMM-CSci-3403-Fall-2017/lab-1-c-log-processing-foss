712f50f58852ba91fe8dee561098c19bdae42eed
Kyle M Foss <fossx229@neoprene.morris.umn.edu>
2017-09-12 10:11:57 -0500

    --Added a file that takes a directory and grabs the data we want, in this case it's the hours the attemps were mad and sorts that data, counts the frequencey of the hours and creates a google chart out of it.

---
+++ bin/create_hours_dist.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#grabs the location of the file we want
+pathName=$(find $1 -type f -name "*failed_login_data.txt")
+
+#grabs the infromation we want, in this case it's users
+awk '{print $3}' $pathName  > intermediate.txt
+
+#sorts the data lexographically and counts the frequencey of the users and prints the data to a javascript fucntion and finally write it to a document.
+sort intermediate.txt | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, " $1"]);";}' > fail.txt
+
+#wraps the text in html tags
+./bin/wrap_contents.sh fail.txt ./html_components/hours_dist $1/hours_dist.html
+#cleaning the mess
+rm fail.txt
+rm intermediate.txt
