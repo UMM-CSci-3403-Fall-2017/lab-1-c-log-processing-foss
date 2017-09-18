d0fc802feac07791992360b76894b9532f40ac87
Kyle M Foss <fossx229@normandy.morris.umn.edu>
2017-09-18 16:54:42 -0500

    --Added a file called process_logs.sh --The file takes in a set of tar files, creates a temp directory called SCRATCH. It extracts the tar files to the that directory, and creates a set of new directorys in that scratch directory named after the computer it came from. The new directories contain logs files from the computers. --Process_logs.sh then runs process_client_logs.sh that generates the output create_username_dist.sh, create_hours_dist.sh, and create_country_dist.sh need. --assemble_report.sh will then run all the outputed html files from create_username_dist.sh, create_hours_dist.sh, and create_country_dist.sh, and write the results to afailed_login_summary.html --process_logs.sh will move failed_login_summary.html to the directory where process_logs is run from.

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
+SCRATCH="SCRATCH"
+mkdir $SCRATCH
 
-
+Current=$(pwd)
 for var in "$@"
 do
+  
     string="$var"
+    string=${string#*/}
    string=${string%_*}
-    #var= awk'{ }'
-    #ext="${$var%%*_}"
-    mkdir $string && tar -xf "$var" -C $string --strip-components 1
+   mkdir $SCRATCH/$string
+  tar -xf "$var" -C $SCRATCH/$string
+ bin/process_client_logs.sh $SCRATCH/$string
+   
 done
 
+bin/create_username_dist.sh $SCRATCH
 
+bin/create_hours_dist.sh $SCRATCH
+
+bin/create_country_dist.sh $SCRATCH
+
+bin/assemble_report.sh $SCRATCH
+
+mv SCRATCH/failed_login_summary.html $Current
+
+#rm -rf SCRATCH
